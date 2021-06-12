602cf76fce6af2be34b4c5b46993b92e1761b9c8
VaidaM1 <83024784+VaidaM1@users.noreply.github.com>
2021-06-12 17:48:59 +0300

    update

--- script.js
+++ script.js
@@ -1,35 +1,49 @@
-document.addEventListener("click", function (event) {
-  var target = event.target;// targetas paclikintas elementas
-  markX(target);
+let payerWins = 0;
+
+document.addEventListener('click', function (event) {
+  var target = event.target; // targetas paclikintas elementas
+  makeMove(target);
 });
 ///when pressed on square the square must be marked as x
-function markX(element) {
-  if (element.hasAttribute("data-cell-index") && element.innerText == "") {
-    element.innerText = "X";
+
+function resetGame() {
+  //all boards should be empty;
+  var cells = Array.from(document.getElementsByClassName("cell"));
+  cells.forEach(xo => xo.innerText = "");
+  //Game status text should be empty;
+  var gameStatusElement = document.querySelector(".game--status");
+  gameStatusElement.innerText = "";
+  //modify button html elementto include onclick;
+}
+
+function makeMove(element) {
+  if (element.classList.contains("cell") && element.innerText == "" && !isGameFinished()){
+    index.innerText = "X";
 
     let finished = isGameFinished();
 
-    if(finished == false)
+    if (finished == false) {
     opponentMove();
     isGameFinished();
 
-   
     //element has special attribute and if
     //edit attribute text to X
-  } else {
+    }
+  } 
+  else {
     //do nothing
   }
 }
-
 
 function opponentMove() {
   // Get all elements with class name
   var cells = Array.from(document.getElementsByClassName("cell")); //atrenkam
   // Filter the elements by just selecting empty ones.
-  cells = cells.filter((cell) => cell.innerText == ""); //filtruojam
+  cells = cells.filter(cell => cell.innerText == ""); //filtruojam
+
   // Get random number
   // let randomNumber = Math.floor(Math.random()*(cells.length))
-  let randomNumber = getRandomNumber(0, cells.length);
+  let randomNumber = Math.floor(Math.random() * (cells.length));
   ////let randomNo = randomInteger(0, elements.lenth);
   // Select random element
   let randomCell = cells[randomNumber];
@@ -37,26 +51,12 @@ function opponentMove() {
   randomCell.innerText = "0";
 }
 
-function getRandomNumber(min, max) {
-  const r = Math.random() * (max - min) + min;
-  return Math.floor(r);
-  //analyses board and calculates the next move
-}
-function isGameFinished() {
-  return isWinnerDetermined() || noMovesLeft();
-}
-
-function whoWon() {
-  //analyses the lines and calculates the winner
-  //
-}
-
 function noMovesLeft() {
   //2.we no longer have any space to play
   var cells = Array.from(document.getElementsByClassName("cell"));
-  cells = cells.filter((cell) => cell.innerText == "");
+  cells = cells.filter(cell => cell.innerText == "");
   if (cells.length == 0) {
-    document.querySelector(".game--status").innerText = "DRAW";
+    var gameStatusElement = document.querySelector(".game--status").innerText = "DRAW";
     //modify game status
     return true; //the game is finished
   }
@@ -65,19 +65,20 @@ function noMovesLeft() {
 
 function isWinnerDetermined() {
   let winningCellCombination = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-  ]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  let isWinnerDetermined = false;
   //go through there winning combinations
-  winningCellCombination.forEach(combination =>
-    {
+  winningCellCombination.forEach(combination => {
     // let i1 = combination[0]; //ilgesnis variantas
     // let i2 = combination[1];
     // let i3 = combination[2];
@@ -86,33 +87,33 @@ function isWinnerDetermined() {
     // let element2 = document.querySelector(`[data-cell-index="${i2}"]`);
     // let element3 = document.querySelector(`[data-cell-index="${i3}"]`);
 
-    elementValue1 = getDataCellTextValueByIndex(combination[0]);
-    elementValue2 = getDataCellTextValueByIndex(combination[1]);
-    elementValue3 = getDataCellTextValueByIndex(combination[2]);
+    elementValue1 = getElementByIndex(combination[0]);
+    elementValue2 = getElementByIndex(combination[1]);
+    elementValue3 = getElementByIndex(combination[2]);
 
+    if (
+      elementValue1 == elementValue2 &&
+      elementValue2 == elementValue3 &&
+      elementValue1 != ""
+    ) {
+      var gameStatusElement = document.querySelector(".game--status");
+      if (elementValue1 == "x") {
+       payerWins++;
+      }
 
-    isGameCompleted = 
-    if(elementValue1 == elementValue2 && elementValue2 == elementValue3 && elementValue1 != ""){
-      var gameStatusElement = document.querySelector("game--status");
+      gameStatusElement.innerText = `${elementValue1} has won the game ${payerWins}`;
 
-       gameStatusElement.innerText = `${elementValue1} has won the game`
-      return true;
+      isWinnerDetermined = true;
     }
-
   });
   //get all cells of those atributes values
   //check all the elements have the same inner text (X or 0)
   //carefull of empty values
 
   //1.we have 3 X or 3 O in a row, column or diagonal
-  return false;
+  return falisWinnerDeterminedse;
 }
 
-function getDataCellTextValueByIndex(index){
+function getElementByIndex(index) {
   return document.querySelector(`[data-cell-index="${index}"]`).innerText;
-}
-
-
-function resetGame() {
-  //
 }
